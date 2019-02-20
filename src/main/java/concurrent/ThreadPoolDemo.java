98ea8a5bfe1c899060a5d8d098baf5254b438d07
lihang <lihang@huobi.com>
2019-02-20 20:53:45 +0800

    2-20 commit

--- src/main/java/concurrent/ThreadPoolDemo.java
+++ src/main/java/concurrent/ThreadPoolDemo.java
@@ -1,11 +1,22 @@
 package concurrent;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ThreadPoolDemo {
     public static void main(String[] args) {
-        Executor pool = Executors.newFixedThreadPool(5);
+        Logger logger = Logger.getLogger(new ThreadPoolDemo().getClass().getName());
 
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+//        ExecutorService pool2 = Executors.newScheduledThreadPool(10);
+
+        for (int i = 0; i < 200; i++) {
+            pool.execute(() -> {
+                logger.log(Level.INFO, Thread.currentThread().getName() + "  is running ...");
+            });
+        }
+        pool.shutdown();
     }
 }
